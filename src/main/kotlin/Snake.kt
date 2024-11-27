6a6523fec375abf41ca4de77a7995e8dd9e29478
btavr <brunotrodrigues98@gmail.com>
2024-11-27 22:19:58 +0000

    Remove mutableList, unico ponto mutabilidade e var game.

--- src/main/kotlin/Snake.kt
+++ src/main/kotlin/Snake.kt
@@ -2,53 +2,60 @@ import pt.isel.canvas.*
 
 data class Snake(val body: List<Position>, val direction: Direction)
 
-const val HEADUP    = "snake|192,0,64,64"
+const val HEADUP = "snake|192,0,64,64"
 const val HEADRIGHT = "snake|256,0,64,64"
-const val HEADDOWN  = "snake|256,64,64,64"
-const val HEADLEFT  = "snake|192,64,64,64"
-const val TAILUP    = "snake|192,128,64,64"
+const val HEADDOWN = "snake|256,64,64,64"
+const val HEADLEFT = "snake|192,64,64,64"
+const val TAILUP = "snake|192,128,64,64"
 const val TAILRIGHT = "snake|256,128,64,64"
-const val TAILDOWN  = "snake|256,192,64,64"
-const val TAILLEFT  = "snake|192,192,64,64"
-var bodyParts       = mutableListOf<String>()
-var snakeHead       = HEADRIGHT
-var snakeTail       = TAILRIGHT
+const val TAILDOWN = "snake|256,192,64,64"
+const val TAILLEFT = "snake|192,192,64,64"
 
-
-    fun Canvas.drawSnake(snake: Snake) {
-        this.erase()
-        bodyParts.add(0, snakeHead )
-        bodyParts.add(bodyParts.lastIndex, snakeTail)
-        this.drawImage(snakeHead, snake.body[0].x * 32, snake.body[0].y * 32,32,32)
-        this.drawImage(snakeTail, snake.body[snake.body.lastIndex].x * 32, snake.body[snake.body.lastIndex].y * 32,32,32)
+fun Canvas.drawSnake(snake: Snake) {
+    this.erase()
+    val headImage = when (snake.direction) {
+        Direction.UP -> HEADUP
+        Direction.DOWN -> HEADDOWN
+        Direction.LEFT -> HEADLEFT
+        Direction.RIGHT -> HEADRIGHT
     }
-
-    fun Snake.move(): Snake {
-        val newHead = nextHeadPosition()
-        return Snake(listOf(newHead) + body.dropLast(1), direction)
+    val tailImage = when (getTailDirection(snake)) {
+        Direction.UP -> TAILUP
+        Direction.DOWN -> TAILDOWN
+        Direction.LEFT -> TAILLEFT
+        Direction.RIGHT -> TAILRIGHT
     }
+    this.drawImage(headImage, snake.body.first().x * 32, snake.body.first().y * 32, 32, 32)
+    this.drawImage(tailImage, snake.body.last().x * 32, snake.body.last().y * 32, 32, 32)
+}
 
-    fun Snake.changeDirection(newDirection: Direction): Snake {
-        when (newDirection) {
-            Direction.UP -> {
-                snakeHead = HEADUP
-                snakeTail = TAILUP
-            }
-            Direction.DOWN -> {
-                snakeHead = HEADDOWN
-                snakeTail = TAILDOWN
-            }
-            Direction.LEFT -> {
-                snakeHead = HEADLEFT
-                snakeTail = TAILLEFT
-            }
-            Direction.RIGHT -> {
-                snakeHead = HEADRIGHT
-                snakeTail = TAILRIGHT
-            }
-        }
-        return Snake(body, newDirection)
+fun getTailDirection(snake: Snake): Direction {
+    // Determine the direction of the tail based on the last two segments
+    if (snake.body.size < 2) return snake.direction // Default to current direction if not enough segments
+
+    val tailEnd = snake.body.last()
+    val beforeTailEnd = snake.body[snake.body.size - 2]
+
+    return when {
+        tailEnd.x < beforeTailEnd.x -> Direction.RIGHT
+        tailEnd.x > beforeTailEnd.x -> Direction.LEFT
+        tailEnd.y < beforeTailEnd.y -> Direction.DOWN
+        else -> Direction.UP
     }
+}
+
+fun Snake.move(): Snake {
+    val newHead = nextHeadPosition()
+    return Snake(listOf(newHead) + body.dropLast(1), direction)
+}
+
+fun Snake.changeDirection(newDirection: Direction): Snake {
+    return if (newDirection != direction.opposite()) {
+        Snake(body, newDirection)
+    } else {
+        this
+    }
+}
 
 fun Snake.nextHeadPosition(): Position {
     val head = body.first()
@@ -58,9 +65,8 @@ fun Snake.nextHeadPosition(): Position {
         Direction.LEFT -> Position(-1, 0)
         Direction.RIGHT -> Position(1, 0)
     }
-    return (head + movement).wrap() // Soma a posição de movimento e aplica o wrap
+    return (head + movement).wrap()
 }
-
 
 enum class Direction {
     UP, DOWN, LEFT, RIGHT;
